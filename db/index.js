06f3c16bff32a6d8015b7e99518084c610fc4c57
Corey Frederickson <coreyfrederickson@Coreys-MacBook-Pro.local>
2021-03-27 10:55:58 -0500

    Started test.db jest

---
+++ db/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+    ...require('./users'), 
+    ...require('./client'), 
+    ...require('./orders'),
+    ...require('./products'),
+    ...require('./reviews'),
+    ...require('./seed') 
+  }
