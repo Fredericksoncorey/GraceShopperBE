fc2ca752a3d10819075758191d7fe08971be2b87
okristenicole9 <okristenicole9@yahoo.com>
2021-04-12 21:30:37 -0500

    working

--- api/products.js
+++ api/products.js
@@ -85,6 +85,7 @@ productsRouter.patch('/update/:productId', admin, async (req, res, next) => {
         res.send({message: "Error: Something went wrong! No update Data Received"})
     }
     const update = {};
+    console.log(req.params)
 
     if (description) {
         update.description = description;
